671b13080177eb6a2fc5d2dc28137dce0724d503
Juancarlos Jauregui <jauregui3@gmail.com>
2017-06-02 21:13:10 -0700

    Begin implementing onClick functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,52 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={props.video} />
+// var App = (props) => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer video={props.video} />
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={props.videos} />
+//     </div>
+//   </div>
+// );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: exampleVideoData[0],
+      videos: exampleVideoData
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+    });
+  }
+
+  render() {
+    return (<div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={this.state.video} />
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={this.state.videos} />
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos} />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+//  render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.isToggleOn ? 'ON' : 'OFF'}
+//       </button>
+//     );
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // TODO: Render the `App` component to the DOM
 
 ReactDOM.render(
-  <App videos={window.exampleVideoData} video={window.exampleVideoData[0]} />, document.getElementById("app")
+  <App />, document.getElementById("app")
   );
 
