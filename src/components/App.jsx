b4efd47f101161433e8765c62d894d84572c4cdc
Juancarlos Jauregui <jauregui3@gmail.com>
2017-06-03 22:15:56 -0700

    Needs handleSearch functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,58 @@
-// var App = (props) => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer video={props.video} />
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList videos={props.videos} />
-//     </div>
-//   </div>
-// );
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      video: exampleVideoData[0],
-      videos: exampleVideoData
+      video: null,
+      videos: [],
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.onFetchVideo = this.onFetchVideo.bind(this);
+    this.searchYouTube = this.props.searchYouTube;
+    this.handleSearchInput = this.handleSearchInput.bind(this);
   }
 
-  onListItemClick() {
-    this.setState({
-    });
+  handleClick(video) {
+    this.setState(prevState => ({
+      video: video
+    }));
+  }
+
+  onFetchVideo (items) {
+    this.setState(prevState => ({
+      video: items[0],
+      videos: items
+    }));
+  }
+
+  componentDidMount() {
+    console.log(this.props);
+    this.props.searchYouTube({}, this.onFetchVideo);
+  }
+
+  handleSearchInput (query) {
+    var options = {
+      query: query
+    };
+
+    this.props.searchYouTube(options, this.onFetchVideo);
   }
 
   render() {
     return (<div>
-      <Nav />
+      <Nav handleSearchInput={handleSearchInput}/>
       <div className="col-md-7">
         <VideoPlayer video={this.state.video} />
       </div>
       <div className="col-md-5">
-        <VideoList videos={this.state.videos} />
+        <VideoList videos={this.state.videos} handleClick={this.handleClick}/>
       </div>
     </div>
     );
   }
+
 }
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-//  render() {
-//     return (
-//       <button onClick={this.handleClick}>
-//         {this.state.isToggleOn ? 'ON' : 'OFF'}
-//       </button>
-//     );
-//   }
-// }
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      part: 'snippet',
+      key: options.key || window.YOUTUBE_API_KEY,
+      q: options.query || 'Hack Reactor',
+      maxResults: options.max || 5,
+      type: 'video',
+      videoEmbeddable: 'true'
+    },
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(data) {
+      throw new Error('Something went wrong, here is what was received', data);
+    }
+  });
+
 };
 
 window.searchYouTube = searchYouTube;
+
