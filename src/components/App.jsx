26836a0be1190ee9646a349a907b1f0da3ebf0d9
Juancarlos Jauregui <jauregui3@gmail.com>
2017-06-04 19:52:45 -0700

    Remove debounce method
    =debounce causes last search test to fail

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,7 +34,7 @@ class App extends React.Component {
 
   render() {
     return (<div>
-      <Nav handleSearchInput={_.debounce(this.fetchVideos.bind(this), 500)}/>
+      <Nav handleSearchInput={this.fetchVideos.bind(this)}/>
       <div className="col-md-7">
         <VideoPlayer video={this.state.video} />
       </div>
