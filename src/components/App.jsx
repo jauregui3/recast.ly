820a81c10f14014df5de4e981e77f0a5e78b2c0b
Juancarlos Jauregui <jauregui3@gmail.com>
2017-06-05 08:35:39 -0700

    Implement debounce on search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
 
   fetchVideos (query) {
     var options = {
-      key: this.API_KEY,
+      key: this.props.API_KEY,
       query: query
     };
 
@@ -34,7 +34,7 @@ class App extends React.Component {
 
   render() {
     return (<div>
-      <Nav handleSearchInput={this.fetchVideos.bind(this)}/>
+      <Nav handleSearchInput={_.debounce(this.fetchVideos.bind(this), 500)}/>
       <div className="col-md-7">
         <VideoPlayer video={this.state.video} />
       </div>
--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-// TODO: Render the `App` component to the DOM
-
-ReactDOM.render(
-  <App searchYouTube={window.searchYouTube}/>, document.getElementById('app')
-);
-
